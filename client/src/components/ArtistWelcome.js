dbeb2fc82a9996602e0919d353ae67c1b9146f90
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-08 16:01:29 +0100

    'refactor: deleted commented out code'

--- client/src/components/ArtistWelcome.js
+++ client/src/components/ArtistWelcome.js
@@ -4,48 +4,6 @@ import Spinner from './Spinner';
 
 
 export default function ArtistWelcome({ user, setUser }) {
-  console.log(user)
-
-  /* const MobileShare = () => {
-    const handleOnClick = () => {
-      if (navigator.share) {
-        navigator
-          .share({
-            title: "`${postTitle} | ${siteTitle}`,",
-            text: `Check out ${postTitle} on ${siteTitle}`,
-            url: document.location.href,
-          })
-          .then(() => {
-            console.log('Successfully shared');
-          })
-          .catch(error => {
-            console.error('Something went wrong sharing the blog', error);
-          });
-      }
-    }; */
-
-  /*   return (
-      <div className='share-icon' onClick={handleOnClick}>
-        <ShareIcon />
-      </div>
-    );
-  }; */
-  // const shareData = {
-  //   title: 'MDN',
-  //   text: 'Learn web development on MDN!',
-  //   url: 'https://developer.mozilla.org'
-  // }
-  // const clickHandler = async () => {
-  //   try {
-  //     await navigator.share(shareData)
-  //     // resultPara.textContent = 'MDN shared successfully'
-  //   } catch (err) {
-  //     // resultPara.textContent = 'Error: ' + err
-  //     console.log(err)
-  //   }
-  // }
-
-
 
   return (
 
