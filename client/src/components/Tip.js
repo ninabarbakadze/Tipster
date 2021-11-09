311c9ce56415e126157b2cba91e490433df37b5f
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 12:48:43 +0100

    refactor: refactored to anonymous functions

--- client/src/components/Tip.js
+++ client/src/components/Tip.js
@@ -1,41 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useHistory } from 'react-router-dom';
-import StripeContainer from '../components/StripeContainer'
 import Spinner from './Spinner';
 
-export default function Tip({ user, setUser, tip, setTip }) {
+const Tip = ({ user, setUser, tip, setTip }) => {
   const history = useHistory();
-
   let { id } = useParams();
 
   useEffect(() => {
-    artistById()
-  }, [])
+    artistById();
+  }, []);
 
- 
-
-
-
-  function artistById() {
+  const artistById = () => {
     fetch(`http://localhost:4000/tip/${id}`, {
       method: 'Get',
       headers: { 'Content-Type': 'application/json' },
     })
       .then(user => user.json())
-      .then(user => setUser(user))
-  }
+      .then(user => setUser(user));
+  };
 
-
-  function clickHandler() {
-    history.push('/message')
-  }
-
-  function handleClick (event) {
-    event.target.style.backgroundColor = "red"
-  }
-
-
+  const clickHandler = () => {
+    history.push('/message');
+  };
 
   return (
     <div>
@@ -45,13 +32,15 @@ export default function Tip({ user, setUser, tip, setTip }) {
         <p id='name'>{user.name}</p>
         <div className='button-parent'>
           <button className='btn' onClick={() => setTip(5)}><span className='tip-span'>$2</span></button>
-          <button className='btn'  onClick={() => setTip(5)}><span className='tip-span'>$5</span></button>
-          <button className='btn'  onClick={() => setTip(10)}><span className='tip-span'>$10</span></button>
+          <button className='btn' onClick={() => setTip(5)}><span className='tip-span'>$5</span></button>
+          <button className='btn' onClick={() => setTip(10)}><span className='tip-span'>$10</span></button>
         </div>
-        <input type="text" placeholder="Custom amount" /* value={tip ? '$' + tip + '.00' : tip} */ onChange={(e) => setTip(e.target.value)}></input>
-    
+        <input type="text" placeholder="Custom amount" onChange={(e) => setTip(e.target.value)}></input>
+
         <button onClick={() => clickHandler()} className='tip-button'><span className='cintinue'>Continue </span></button>
       </div> : <Spinner />}
     </div>
-  )
-}
+  );
+};
+
+export default Tip;
