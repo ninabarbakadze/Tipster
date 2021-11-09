73712424fba6d921aba530e9ca080374f6a1c2f5
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 14:09:00 +0100

    refactor: removed trailing whitespace

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require('cors');
 const router = require('./router');
 const db = require('./models/model');
-
 const session = require('express-session');
 
 app.use(session({
@@ -12,15 +11,11 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-
 app.use(express.json());
 app.use(cors());
 app.use(router);
-
-// const populateUser = require('./mock-data/populateUser');
 
 (async function bootstrap () {
   await db.sequelize.sync();
   app.listen(4000);
 })();
-
