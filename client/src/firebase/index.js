2686bd0e176dd8224072b568d2f98b2f96d80e79
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 11:55:22 +0100

    refactor: changed double quotes to single

--- client/src/firebase/index.js
+++ client/src/firebase/index.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPLuMeAHIiSO1douAaEi7p1nYtl2JlSmU",
-  authDomain: "tipster-3d872.firebaseapp.com",
-  projectId: "tipster-3d872",
-  storageBucket: "tipster-3d872.appspot.com",
-  messagingSenderId: "309594397293",
-  appId: "1:309594397293:web:3bd68b8919884a67adbd64",
-  measurementId: "G-0030BK3G4Q"
+  apiKey: 'AIzaSyCPLuMeAHIiSO1douAaEi7p1nYtl2JlSmU',
+  authDomain: 'tipster-3d872.firebaseapp.com',
+  projectId: 'tipster-3d872',
+  storageBucket: 'tipster-3d872.appspot.com',
+  messagingSenderId: '309594397293',
+  appId: '1:309594397293:web:3bd68b8919884a67adbd64',
+  measurementId: 'G-0030BK3G4Q'
 };
 
 const firebase = initializeApp(firebaseConfig);
