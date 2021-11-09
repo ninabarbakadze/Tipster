3cc3a260790e7d3dfdcc0d5cfc3b37e2be9c3c22
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 12:02:41 +0100

    refactor: removing trailing whitespace

--- client/src/components/ArtistDashboard.jsx
+++ client/src/components/ArtistDashboard.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import TipDashboard from './TipDashboard';
 import { Link } from 'react-router-dom';
-import Spinner from './Spinner'
+import Spinner from './Spinner';
 
- const ArtistDashboard = ({logOut, user, tips, setTips}) => {
+const ArtistDashboard = ({ logOut, user, tips, setTips }) => {
 
   return (
-  <div>
-        {user  ? <div className='container'>
-   <div className='header'>
-     <h1>TIPSTER</h1>
-     <Link to='/' >
- <button onClick={() => logOut()}><span>Sign out</span></button>
-     </Link>
-   </div>
-   <div>
-     <img className='photo-artist' src={user.photo} alt='img'/>
-     <img className='photo-qr' src={user.qrcode} alt='img'/>    
-   </div>
-   <p className='artist-name'>{user.name}</p>
-   
-   <TipDashboard user={user} tips={tips} setTips={setTips}></TipDashboard> 
-    </div> : <Spinner /> }
-    </div>  
-  )
-}
+    <div>
+      {user ? <div className='container'>
+        <div className='header'>
+          <h1>TIPSTER</h1>
+          <Link to='/' >
+            <button onClick={() => logOut()}><span>Sign out</span></button>
+          </Link>
+        </div>
+        <div>
+          <img className='photo-artist' src={user.photo} alt='img' />
+          <img className='photo-qr' src={user.qrcode} alt='img' />
+        </div>
+        <p className='artist-name'>{user.name}</p>
+        <TipDashboard user={user} tips={tips} setTips={setTips}></TipDashboard>
+      </div> : <Spinner />}
+    </div>
+  );
+};
 
-export default ArtistDashboard
+export default ArtistDashboard;
