124bda8143f2d8c01182d2c06a2bee22e19380b0
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 12:44:13 +0100

    refactor: removed console.logs and refactored to anonymous functions

--- client/src/components/StripeButton.jsx
+++ client/src/components/StripeButton.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import Stripe from 'stripe';
-import { browserHistory } from "react-router";
 
-export default function StripeButton({ user }) {
-  console.log('stripe button', user);
+const StripeButton = ({ user }) => {
 
-  function handleClick(e) {
-    // e.preventDefault()
-    stripeSignUp()
-  }
-  
-  function stripeSignUp() {
-    fetch("http://localhost:4000/onboard-user", {
-      method: "POST",
+  const stripeSignUp = () => {
+    fetch('http://localhost:4000/onboard-user', {
+      method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(user)
     })
@@ -20,17 +12,18 @@ export default function StripeButton({ user }) {
       .then(data => {
         if (data.url) {
           window.location = data.url;
-        } else {
         }
       }).catch(err => {
         console.error(err);
       });
-  }
+  };
 
   return (
     <div className='stripe-button'>
       <p>First, set up your payment method to receive tips.</p>
-      <button onClick={() => handleClick()}><span>Let's go</span></button>
+      <button onClick={() => stripeSignUp()}><span>Let's go</span></button>
     </div>
   );
-}
+};
+
+export default StripeButton;
