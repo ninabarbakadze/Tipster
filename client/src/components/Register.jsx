b6c73c5d2f1a48ca64438eb40a1d8cd3980dd31a
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 12:11:51 +0100

    refactor: refactored to anonymous functions

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,67 +1,52 @@
 import '../App.css';
 import React, { useState, useEffect } from 'react';
 import ImageUpload from './ImageUpload';
-import { Link, useHistory } from 'react-router-dom';
-import { ref, uploadBytes } from "firebase/storage";
+import { useHistory } from 'react-router-dom';
+import { ref, uploadBytes } from 'firebase/storage';
 import { storage } from '../firebase';
 
-// import { Route } from 'react-router-dom';
-
-// import { firebaase } from 'firebase';
-
-
-export default function Register({ user, setUser, ...props }) {
+const Register = ({ user, setUser, ...props }) => {
   const [file, setFile] = useState();
-  const [url, setUrl] = useState()
+  const [url, setUrl] = useState();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
 
-
-  function handleUpload() {
-    console.log('handleupload fired')
+  const handleUpload = () => {
+    console.log('handleupload fired');
     const storageRef = ref(storage, `/images/${file.name}`);
     uploadBytes(storageRef, file).then((snapshot) => {
       setUrl(`https://firebasestorage.googleapis.com/v0/b/tipster-3d872.appspot.com/o/${snapshot.ref._location.path_.replace(/\//, '%2F')}?alt=media`);
     });
-  }
-
+  };
 
   const handleSubmit = (e) => {
-    console.log('handlesubmit fired')
+    console.log('handlesubmit fired');
     e.preventDefault();
-    handleUpload()
-  }
-
-
+    handleUpload();
+  };
 
   useEffect(() => {
     if (url && name && email && password) {
-      createUser({ name, email, password, photo: url })
+      createUser({ name, email, password, photo: url });
     }
-    //navigate to artist page 
-    // <Link to='/artist'> </Link>
-    //on artist page have a conditional render ternary operator if user is not undifined render otherwise loading spinner
-  }, [url])
+  }, [url]);
 
-  function createUser(user) {
+  const createUser = (user) => {
     console.log(user);
-    fetch(`http://localhost:4000/register`, {
+    fetch('http://localhost:4000/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(user)
     })
       .then(res => res.json())
       .then(user => {
-        console.log('createUser', user)
-        setUser(user)
-        history.push('/connect-to-stripe')
-      })
-  }
-
-  console.log('user from register', user)
-
+        console.log('createUser', user);
+        setUser(user);
+        history.push('/connect-to-stripe');
+      });
+  };
 
   return (
     <div className="register">
@@ -79,4 +64,6 @@ export default function Register({ user, setUser, ...props }) {
       </div>
     </div>
   );
-}
+};
+
+export default Register;
