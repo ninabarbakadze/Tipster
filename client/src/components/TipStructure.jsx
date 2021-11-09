510341fa36375cf59efcb7703c755d5ce3574aa5
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 12:55:53 +0100

    refactor: to anonymous functions and removed trailing whitespace

---
+++ client/src/components/TipStructure.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import moment from 'moment';
+
+const Event = ({ tip }) => {
+
+  return (
+    <div className='tip'>
+      <div className='name-amount'>
+        <p>{tip.name}</p>
+        <p className='amount'>+ ${tip.amount}.00</p>
+      </div>
+      <div className='message-time'>
+        <p>{JSON.stringify(tip.message)}</p>
+        <p className='time'>{moment(tip.createdAt).startOf('hour').fromNow()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Event;
