3dac117b454dc5e05bd4fd9c876bc16428af4fe2
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 11:59:23 +0100

    Update ArtistWelcome.js

--- client/src/components/ArtistWelcome.js
+++ client/src/components/ArtistWelcome.js
@@ -2,23 +2,21 @@ import '../App.css';
 import React, { useEffect } from 'react';
 import Spinner from './Spinner';
 
-
  const ArtistWelcome = ({ user, setUser }) => {
 
-  return (
-
-    <div className="artist-dashboard">
+  return 
+      <div className="artist-dashboard">
       {user ? <>
         <h1>TIPSTER</h1>
-        <img className='artist-photo' src={user.photo} alt='artist' />
-        <p className='artist-name'>{user.name}</p>
-        <p className='start'>To start receiving tips, <br></br> share or print your QR code.</p>
-        <img className='qr' src={user.qrcode} alt='qrcode' />
-        <button className='share'><span>Share  􀈂</span></button>
-        <button className='print'><span>Print  􀎚</span></button>
+        <img className="artist-photo" src={user.photo} alt="artist' />
+        <p className="artist-name">{user.name}</p>
+        <p className="start">To start receiving tips, <br></br> share or print your QR code.</p>
+        <img className="qr" src={user.qrcode} alt="qrcode" />
+        <button className="share"><span>Share  􀈂</span></button>
+        <button className="print"><span>Print  􀎚</span></button>
       </> : <Spinner />}
-
     </div >
   );
 }
+ 
 export default ArtistWelcome;
