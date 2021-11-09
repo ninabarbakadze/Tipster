34ec3c1ea61e864e3e542e2cfd8b61dbbf0b6767
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-09 12:47:23 +0100

    Update and rename StripeContainer.js to StripeContainer.jsx

--- client/src/components/StripeContainer.jsx
+++ client/src/components/StripeContainer.jsx
@@ -3,15 +3,15 @@ import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import PaymentForm from './PaymentForm';
 
-
 const PUBLIC_KEY = 'pk_test_51JaGGqHCv9NHRvAH2nJPK70AIZmF2DhJe4Xd1At34BJoIdwipCcKqNkytp5iqr7bCltkxMvUga3O4uUOmxu8brpN00Q0Ex8oDT'
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
-export default function StripeContainer ({ tip, user }) {
-  console.log('stripe container', user)
+const StripeContainer =  ({ tip, user }) {
   return (
     <Elements stripe={stripeTestPromise}>
       <PaymentForm tip={tip} user={user} />
     </Elements>
-  )
-}
+  );
+};
+
+export default StripeContainer;
